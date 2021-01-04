3a69dce5181c6a75c30b74d6c65dc95b8b6e1e2e
tomaszewskim <tomaszewski_m@hotmail.com>
2021-01-04 12:10:41 +0100

    fix crypto validation

--- commands/crypto.js
+++ commands/crypto.js
@@ -1,15 +1,16 @@
 const coinTicker = require('coin-ticker');
 
 module.exports = {
-    name: "crypto",
-    description: "cena danej kryptowaluty",
+    name: 'crypto',
+    description: 'cena danej kryptowaluty',
     execute(message, args) {
         const ticker = args[0].toUpperCase();
         coinTicker('bitfinex', `${ticker}_USD`).then(data => {
-            message.channel.send(`> ${ticker} Price: $${parseFloat(data.last).toFixed(2)}`);
-        }).catch(err => {
-            console.log(err);
-            message.channel.send("Nieprawidłowy ticker.")
-        });
+            if (!isNaN(data.last)) {
+                message.channel.send(`> ${ticker} Price: $${parseFloat(data.last).toFixed(2)}`);
+            } else {
+                message.channel.send('Nieprawidłowy ticker.');
+            }
+        })
     }
 }
